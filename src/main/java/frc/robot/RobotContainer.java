7a0c01b96ccb524de5cdc89b3b2f9819a641f289
OrigamiYoda <52184332+OrigamiYoda@users.noreply.github.com>
2023-02-18 20:38:18 -0500

    Path Planner Command Robot v2.0

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,10 +4,18 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+
+import frc.robot.Constants.OIConstants;
+import frc.robot.commands.SwerveJoystickCmd;
+import frc.robot.commands.Auton.ThreeElement;
+import frc.robot.commands.Auton.line2meters;
+import frc.robot.commands.Auton.line2metersCommand;
+import frc.robot.subsystems.SwerveSubsystem;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -20,17 +28,50 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  // private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  // private final CommandXboxController m_driverController =
+  //     new CommandXboxController(OperatorConstants.kDriverControllerPort);
+
+    private static final XboxController cont = new XboxController(Constants.controllerPort);
+
+    public static final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
+
+    private final Joystick driverJoystick = new Joystick(OIConstants.kDriverControllerPort);
+      
+    Command autonomousCommand;
+    SendableChooser<Command> chooser = new SendableChooser<>();
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
-    configureBindings();
+    swerveSubsystem.setDefaultCommand(new SwerveJoystickCmd(
+      swerveSubsystem,
+      () -> -driverJoystick.getRawAxis(OIConstants.kDriverYAxis),
+      () -> driverJoystick.getRawAxis(OIConstants.kDriverXAxis),
+      () -> driverJoystick.getRawAxis(OIConstants.kDriverRotXAxis),
+      () -> driverJoystick.getRawAxis(OIConstants.kDriverRotYAxis),      
+      () -> !driverJoystick.getRawButton(OIConstants.kDriverFieldOrientedButtonIdx), 
+      () -> driverJoystick.getPOV(), 
+      () -> driverJoystick.getRawAxis(OIConstants.kDriverLeftTrigger), 
+      () -> driverJoystick.getRawAxis(OIConstants.kDriverRightTrigger), 
+      () -> driverJoystick.getRawButton(Constants.buttonY), 
+      () -> driverJoystick.getRawButton(OIConstants.kDriverCancelTurn), 
+      () -> driverJoystick.getRawButton(OIConstants.kDriverTopSpeed)));
+      // Configure the button bindings
+    configureButtonBindings();
+
+
+    chooser.setDefaultOption("Line 2 Meters", new line2meters(swerveSubsystem));
+    chooser.addOption("Auto Three Element", new ThreeElement(swerveSubsystem));
+    chooser.addOption("Line 2 Meters Command", new line2metersCommand());
+
+
+    Shuffleboard.getTab("Autonomous").add(chooser);
   }
+
+
 
   /**
    * Use this method to define your trigger->command mappings. Triggers can be created via the
@@ -41,15 +82,7 @@ public class RobotContainer {
    * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
-  private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
-  }
+  private void configureButtonBindings() {}
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -57,7 +90,10 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return chooser.getSelected();
+  }
+
+  public static SwerveSubsystem getSwerveSubsystem(){
+    return swerveSubsystem;
   }
 }
